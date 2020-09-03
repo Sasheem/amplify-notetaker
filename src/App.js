0d50828f315bd4f8764150a8b605b9a45d1f74d6
Sasheem <sts12@me.com>
2020-09-03 15:33:58 -0400

    Added app ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 import '@aws-amplify/ui/dist/style.css';
 
 function App() {
-	return <div>App</div>;
+	const [notes, setNotes] = useState([{ id: 1, note: 'hello world ' }]);
+	return (
+		<div className='flex flex-column items-center justify-center pa3 bg-washed-red'>
+			<h1 className='code f2-l'>Amplify Notetaker</h1>
+			{/* Note Form */}
+			<form className='mb3'>
+				<input type='text' className='pa2 f4' placeholder='Write your note' />
+				<button type='submit' className='pa2 f4'>
+					Add Note
+				</button>
+			</form>
+
+			{/* Notes List */}
+			<div>
+				{notes.map((item) => (
+					<div key={item.id} className='flex items-center'>
+						<li className='list pa1 f3'>{item.note}</li>
+						<button className='bg-transparent bn f4'>
+							<span>&times;</span>
+						</button>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default withAuthenticator(App, { includeGreetings: true });
