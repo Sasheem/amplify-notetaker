fd8675300b084858189d2fb1438039444b2a0566
Sasheem <sts12@me.com>
2020-09-09 13:29:41 -0400

    executing graphql queries / mutations with AppSync

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
+import { createNote } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
+
 import '@aws-amplify/ui/dist/style.css';
 
 function App() {
-	const [notes, setNotes] = useState([{ id: 1, note: 'hello world ' }]);
+	const [note, setNote] = useState('');
+	const [notes, setNotes] = useState([]);
+
+	const handleNoteChange = (event) => setNote(event.target.value);
+
+	const handleNoteSubmit = async (event) => {
+		event.preventDefault();
+		const input = { note };
+		const result = await API.graphql(graphqlOperation(createNote, { input }));
+		const newNote = result.data.createNote;
+		setNotes([...notes, newNote]);
+		setNote('');
+	};
+
+	// useEffect behaves like componentDidMount + componentDidUnmount + componentDidUpdate all combined
+	useEffect(() => {
+		async function fetchNotes() {
+			const result = await API.graphql(graphqlOperation(listNotes));
+			setNotes(result.data.listNotes.items);
+		}
+
+		fetchNotes();
+	}, []);
+
 	return (
 		<div className='flex flex-column items-center justify-center pa3 bg-washed-red'>
 			<h1 className='code f2-l'>Amplify Notetaker</h1>
 			{/* Note Form */}
-			<form className='mb3'>
-				<input type='text' className='pa2 f4' placeholder='Write your note' />
+			<form onSubmit={handleNoteSubmit} className='mb3'>
+				<input
+					type='text'
+					className='pa2 f4'
+					onChange={handleNoteChange}
+					value={note}
+					placeholder='Write your note'
+				/>
 				<button type='submit' className='pa2 f4'>
 					Add Note
 				</button>
